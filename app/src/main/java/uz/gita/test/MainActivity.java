9804f83760873651e533a8bba0dcfb8d538bf4cf
Zulfiqor0912 <zulfiqorrustamov81@gmail.com>
2023-03-25 15:04:01 +0500

    deleted_component

--- app/src/main/java/uz/gita/test/MainActivity.java
+++ app/src/main/java/uz/gita/test/MainActivity.java
@@ -74,12 +74,6 @@ public class MainActivity extends AppCompatActivity {
         dialog.setCancelable(false);
         dialog.show();
 
-        Dialog dialog = new Dialog(this);
-        dialog.setContentView(R.layout.activity_about);
-        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        dialog.setCancelable(false);
-        dialog.show();
-
         AppCompatButton btnBack = dialog.findViewById(R.id.btnBack);
 
         btnBack.setOnClickListener(view1 -> {
